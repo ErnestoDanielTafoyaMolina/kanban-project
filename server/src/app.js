7a04562c0733dd2d316c1b897cef6f70af323b8e
ErnestoDanielTafoyaMolina <tafoya.ernesto.1cm@gmail.com>
2023-06-24 13:05:02 -0600

    toProd/

--- server/src/app.js
+++ server/src/app.js
@@ -4,11 +4,11 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import config from "./config";
-
+import cookieParser from "cookie-parser";
 //import routes
 
 import TasksRoutes from "./routes/tasks.routes";
-
+import AuthRoutes from "./routes/auth.routes";
 //init app
 const app = express();
 app.set("port",config.port);
@@ -16,11 +16,14 @@ app.set("port",config.port);
 app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 //use routes
 
 app.use("/api",TasksRoutes);
+app.use("/api",AuthRoutes);
 
+app.use("/*",(req,res)=>res.status(404).send("bad request"));
 export default app;
